99c5efa0ccd18c37e70d74511086c0b402c0f541
marc0-03 <60389027+marc0-03@users.noreply.github.com>
2023-03-29 11:44:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vite-Vue_test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-mq4zr6)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <va-button color="danger" class="mr-6 mb-2">Shiiiii, a button</va-button>
+
+  <h3>And now another UI Element</h3>
+
+  <va-image class="w-full md:w-1/2 lg:w-1/2" src="https://picsum.photos/1500"/>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
